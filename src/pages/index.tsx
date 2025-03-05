2eddb9fb7ac455ea122313c2c9935373bccdc159
Hanne <hanne.haakaas@gmail.com>
2025-03-05 22:10:18 +0100

    Create: React project with Typescript and Tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import "@/styles/globals.css";
+import React from "react";
+
+export default function Home () {
+    return (
+        <main className="w-screen h-screen bg-[#DDD7D1] flex justify-center pt-8">
+            <div className="text-4xl">
+                Home page
+            </div>
+        </main>
+    )
+}
