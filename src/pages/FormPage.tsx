103a23cd2050e83f99b2196b5790fbe0e2dd9216
August Solli Middelkoop <august.kampen@gmail.com>
2025-03-06 18:58:00 +0100

    Feature: Temporary frontend for forms.

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -1,11 +1,9 @@
-import Navbar from "@/components/shared/Navbar";
 import Form from "@/components/formpage/Form";
 import "@/styles/globals.css";
 
 export default function FormPage(){
     return (
         <>
-            <Navbar></Navbar>
             <Form></Form>
         </>
     )
