4838f4fc6cc06a36066fb5467a29d8c6af950443
August Solli Middelkoop <august.kampen@gmail.com>
2025-03-06 17:51:16 +0100

    Feature: Added FormPage with fucntionality to jump back and forth from question.

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return(
+        <>
+            <nav className="">
+                <h2>Orbit</h2>
+
+            </nav>
+        </>
+        )
+}
---
+++ src/pages/FormPage.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/shared/Navbar";
+import Form from "@/components/formpage/Form";
+import "@/styles/globals.css";
+
+export default function FormPage(){
+    return (
+        <>
+            <Navbar></Navbar>
+            <Form></Form>
+        </>
+    )
+}
