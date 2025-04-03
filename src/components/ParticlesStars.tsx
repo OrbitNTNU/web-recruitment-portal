36846ad7546a5a80242e70529748f619ba3ea6e8
Jayan Thayananthan <jayant@stud.ntnu.no>
2025-04-03 12:19:08 +0200

    feat: Add all steps and particles-background
    Add alle steps and backgroud particles. Need to fix session-storage and also make both particles be used in the same backround.

---
+++ src/components/ParticlesStars.tsx
@@ -0,0 +1,94 @@
+import { useCallback } from "react";
+import { loadSlim } from "tsparticles-slim";
+import { Engine } from "tsparticles-engine";
+import Particles from "react-tsparticles";
+
+export default function ParticlesStars() {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadSlim(engine);
+  }, []);
+
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      options={{
+        autoPlay: true,
+        background: {
+          color: "transparent",
+        },
+        fullScreen: {
+          enable: false,
+      
+        },
+        detectRetina: true,
+        fpsLimit: 120,
+        interactivity: {
+          detectsOn: "window",
+          events: {
+            onClick: {
+              enable: true,
+              mode: "repulse",
+            },
+            onHover: {
+              enable: true,
+              mode: "bubble",
+              parallax: {
+                enable: false,
+                force: 2,
+                smooth: 10,
+              },
+            },
+            resize: {
+              enable: true,
+              delay: 0.5,
+            },
+          },
+        },
+        particles: {
+          number: {
+            density: {
+              enable: true,
+              width: 1920,
+              height: 1080,
+            },
+            value: 110,
+          },
+          color: {
+            value: "#ffffff",
+          },
+          opacity: {
+            value: { min: 0.1, max: 1 },
+            animation: {
+              enable: true,
+              speed: 1,
+              sync: false,
+              startValue: "random",
+            },
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 3 },
+            animation: {
+              enable: false,
+            },
+          },
+          move: {
+            enable: true,
+            speed: { min: 0.1, max: 0.4 },
+            direction: "none",
+            random: false,
+            straight: false,
+            outModes: {
+              default: "out",
+            },
+          },
+        },
+        pauseOnBlur: true,
+        pauseOnOutsideViewport: true,
+      }}
+    />
+  );
+}
--- src/utilities/gradientColors.ts
+++ src/utilities/gradientColors.ts
@@ -2,22 +2,10 @@ export const generateGradientColors = (numTeams: number, centerIndex: number) =>
   const colors = [];
 
   for (let i = 0; i < numTeams; i++) {
-    let red = 0,
-      green = 0,
-      blue = 0;
-    const distance = (i - centerIndex) / (numTeams * 0.4);
+    const distance = Math.abs(i - centerIndex) / (numTeams * 0.5); 
+    const intensity = Math.max(0, Math.min(255, Math.floor(255 * (1 - distance)))); 
 
-    if (Math.abs(distance) < 0.05) {
-      green = 255;
-    } else if (distance < 0) {
-      blue = 255;
-      green = Math.max(0, 255 - Math.floor(255 * (Math.abs(distance) * 2.5)));
-    } else {
-      red = 255;
-      green = Math.max(0, 255 - Math.floor(255 * (distance * 2.5)));
-    }
-
-    colors.push(`rgb(${red}, ${green}, ${blue})`);
+    colors.push(`rgb(${intensity}, ${intensity}, ${intensity})`);
   }
 
   return colors;
