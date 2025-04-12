062a83a7c463b5667b506a6a481edf9d58cf7dd5
Syed Wahib Hussain <137010664+Wasabiy@users.noreply.github.com>
2025-04-12 12:41:10 +0200

    chore: pop previous stash and commit current merge from dev

--- src/interfaces/application.ts
+++ src/interfaces/application.ts
@@ -1,15 +1,34 @@
-import { type position } from "./position";
+import {Position} from "@/interfaces/position";
 
-export interface ApplicationValues {
-  fullName: string;
-  username: string;
-  email: string;
+export interface Application {
+  name: string;
+  personalEmail: string;
+  ntnuUsername: string;
   phoneNumber: string;
-  emailAddress: string;
   fieldOfStudy: string;
   yearOfStudy: number;
-  position: position[];
-  previousExperience: string;
-  personalInfo: string;
+  experience: string;
+  description: string;
   submissionDate: string;
+  saveApplication: boolean;
+}
+
+export interface Props {
+  applications: Application[];
+}
+
+
+export interface ApplicationWithPositions {
+  name: string;
+  personalEmail: string;
+  ntnuUsername: string;
+  phoneNumber: string;
+  fieldOfStudy: string;
+  yearOfStudy: number;
+  experience: string;
+  description: string;
+  submissionDate: string;
+  saveApplication: boolean;
+  positions: Position[];
+
 }
