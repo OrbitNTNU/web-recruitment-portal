3b5715257445a21ea4ac4c6ce6b61c2219b133ea
Hanne <hanne.haakaas@gmail.com>
2025-10-09 21:05:38 +0200

    Migrate table state to zustand

---
+++ src/stores/TableStore/UseTableStore.ts
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+import { useEffect } from "react";
+
+interface TableValues {
+    columns: string[];
+    setColumns: (columns: string[]) => void;
+}
+
+const SESSIONSTORAGE_NAME = "tableState";
+
+export const useTableStore = create<TableValues>((set) => ({
+    columns: [
+        "ID",
+        "name",
+        "fieldOfStudy",
+        "yearOfStudy",
+        "positions"
+    ],
+    setColumns: (columns) => set({ columns }),
+}));
+
+const loadState = (): Partial<TableValues> => {
+    if (typeof window === "undefined") return {}
+
+    const savedState = sessionStorage.getItem(SESSIONSTORAGE_NAME);
+    return savedState ? (JSON.parse(savedState) as Partial<TableValues>) : {};
+}
+
+export const useSessionStorageSync = () => {
+    const tableState = useTableStore();
+    console.log("hei");
+
+    useEffect(() => {
+        const savedState = loadState();
+        useTableStore.setState(savedState);
+    }, []);
+
+    useEffect(() => {
+        sessionStorage.setItem(SESSIONSTORAGE_NAME, JSON.stringify(tableState));
+    }, [tableState]);
+}
