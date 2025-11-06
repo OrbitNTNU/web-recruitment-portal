9730578ade56c7c3b38c7e18440ff90b350e32be
Jayan Thayananthan <jayanthaya@outlook.com>
2025-11-06 06:40:55 +0100

    feat: new introVideo with logo and also add new 3D earth model with satellite using three

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,8 +2,7 @@ import * as grpc from "@grpc/grpc-js";
 import path from "path";
 import type { GetServerSideProps } from "next";
 import { loadProto } from "@/client/loadProto";
-import type { Props } from "@/interfaces/application";
-import type { Application } from "@/interfaces/application";
+import type { Application, Props } from "@/interfaces/application";
 
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
   const GREETER_PROTO_PATH = path.resolve(
