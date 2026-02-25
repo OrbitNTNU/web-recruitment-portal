dfce79fd94f0598a58f778e2d34026bbb1d70fd9
Jayan Thayananthan <jayanthaya@outlook.com>
2026-02-25 16:01:41 +0100

    feat: add speedinsights

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
-import React from "react";
-import HomePage from "./HomePage/HomePage";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-export default function Home() {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <HomePage />
-  )
+    <div>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </div>
+  );
 }
